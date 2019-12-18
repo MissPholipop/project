2b320181b88d02d16dfa5d069de870a50e3dc5a4
MissPholipop <1067927306@qq.com>
2019-12-18 11:06:37 +0800

    数据绑定学习

--- pages/data/data.js
+++ pages/data/data.js
@@ -1,8 +1,13 @@
-var helloData = {
-  name: 'WeChat'
+var message = {
+  name: 'WeChat',
+  array: [1, 2, 3, 4, 5],
+  view: 'MINA',
+  staffA: {firstName: 'Hulk', lastName: 'Hu'},
+  staffB: {firstName: 'Shang', lastName: 'You'},
+  staffC: {firstName: 'Gideon', lastName: 'Lin'}
 }
 Page({
-data: helloData,
+data: message,
   changeName: function(e) {
     // sent data change to view.
     this.setData({
