84edc3bf1b4409cdf6e885b871e46f4f05393d2b
MissPholipop <1067927306@qq.com>
2019-12-18 14:16:34 +0800

    tabBar 消失

---
+++ pages/head/header.js
@@ -0,0 +1,4 @@
+var message = {}
+Page({
+data: message,
+})
--- pages/hyan/hyan.js
+++ pages/hyan/hyan.js
@@ -1,6 +1,11 @@
 var util = require('../../utils/util.js')
 var helloData = {
-  name: 'WeChat'
+  name: 'WeChat',
+  item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2019-12-18'
+    }
 }
 Page({
 data: helloData,
@@ -9,5 +14,29 @@ data: helloData,
     this.setData({
       name: 'MINA'
     })
+  },
+  handleTap1:function(e){
+     console.log(e)
+    
+    this.setData({
+      name: 'handleTap1'
+    })
+  },
+  handleTap2:function(e){
+     console.log(e)
+    this.setData({
+      name: 'handleTap2'
+    })
+  },
+  handleTap3:function(e){
+    console.log(e)
+    this.setData({
+      name: 'handleTap3'
+    })
+  },
+    bindViewTap:function(event){
+    event.target.dataset.alphaBeta === 1 // - 会转为驼峰写法
+    event.target.dataset.alphabeta === 2 // 大写会转为小写
   }
+
 })
